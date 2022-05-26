2c18467ea2406cdecaeee616bea0d1fa659a6349
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2022-05-26 10:04:37 +0200

    enhance hyperparameters.py and hyperparameters_range.py

--- src/hyperparameters/hyperparameters.py
+++ src/hyperparameters/hyperparameters.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
+import torch
+
 from collections import OrderedDict
 
 
 class Hyperparameters:
+    """
+    class to hold hyperparameters
+    """
+
     def __init__(self):
+        """
+        constructor for hyperparameters class
+        """
         hp_dict = OrderedDict()
-        # e.g. hp_dict["a_hyperparameter"] = 1
+
+        hp_dict["episodes"] = 7
+        hp_dict["steps_per_episode"] = 3
+
+        hp_dict["gamma"] = 0.995
+        hp_dict["clip"] = 0.200
+
+        hp_dict["actor_layers"] = [128, 64, 32]
+        hp_dict["actor_activation_function"] = torch.nn.ReLU
+        hp_dict["actor_output_function"] = torch.nn.Tanh
+        hp_dict["actor_optimizer"] = torch.optim.Adam
+        hp_dict["actor_optimizer_learning_rate"] = 0.005
+
+        hp_dict["critic_layers"] = [128, 64, 32]
+        hp_dict["critic_activation_function"] = torch.nn.ReLU
+        hp_dict["critic_output_function"] = torch.nn.Tanh
+        hp_dict["critic_optimizer"] = torch.optim.Adam
+        hp_dict["critic_optimizer_learning_rate"] = 0.005
+
         self.hp_dict = hp_dict
 
     def get_dict(self) -> OrderedDict:
+        """
+        function to get hyperparameters as an OrderedDict
+        :return: hyperparameters in an OrderedDict
+        """
         return self.hp_dict
--- src/hyperparameters/hyperparameters_range.py
+++ src/hyperparameters/hyperparameters_range.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
 
+import torch
+
 from collections import OrderedDict
 
 
 class HyperparametersRange:
+    """
+    class to hold hyperparameter ranges
+    """
+
     def __init__(self):
-        hp_dict = OrderedDict()
-        # e.g. hp_dict["a_hyperparameter"] = [1, 2, 4]
-        self.hp_dict = hp_dict
+        """
+        constructor for hyperparameter ranges class
+        """
+        hpr_dict = OrderedDict()
+        hpr_dict["episodes"] = [7]
+        hpr_dict["steps_per_episode"] = [3]
+
+        hpr_dict["gamma"] = [0.995]
+        hpr_dict["clip"] = [0.200]
+
+        hpr_dict["actor_layers"] = [[128, 64, 32]]
+        hpr_dict["actor_activation_function"] = [torch.nn.ReLU]
+        hpr_dict["actor_output_function"] = [torch.nn.Tanh]
+        hpr_dict["actor_optimizer"] = [torch.optim.Adam]
+        hpr_dict["actor_optimizer_learning_rate"] = [0.005]
+
+        hpr_dict["critic_layers"] = [[128, 64, 32]]
+        hpr_dict["critic_activation_function"] = [torch.nn.ReLU]
+        hpr_dict["critic_output_function"] = [torch.nn.Tanh]
+        hpr_dict["critic_optimizer"] = [torch.optim.Adam]
+        hpr_dict["critic_optimizer_learning_rate"] = [0.005]
+
+        self.hpr_dict = hpr_dict
 
     def get_dict(self) -> OrderedDict:
-        return self.hp_dict
+        """
+        function to get hyperparameter ranges as an OrderedDict
+        :return: hyperparameter ranges in an OrderedDict
+        """
+        return self.hpr_dict
